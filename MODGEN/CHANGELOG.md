cb748454ea368beb4f21abfad1153302e5dbd97b
Turbero <dicon18@gmail.com>
2026-04-20 01:08:18 +0200

    Updating to version 1.0.1

--- MODGEN/CHANGELOG.md
+++ MODGEN/CHANGELOG.md
@@ -1,5 +1,11 @@
 ### CHANGELOG
 
+## 1.0.1
+
+* Fixed timer after dying in permadeath. Now it will stop and be colored in red.
+* Fixed issue when stopping the game, the menu was resuming immediately by mistake
+* Speedrun menu can be opened while sitting now
+
 ## 1.0.0
 
 Initial version
--- SpeedrunManager.cs
+++ SpeedrunManager.cs
@@ -12,7 +12,7 @@ namespace SpeedrunManager
     {
         public const string GUID = "Turbero.SpeedrunManager";
         public const string NAME = "Speedrun Manager";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.1";
 
         private readonly Harmony harmony = new Harmony(GUID);
         
